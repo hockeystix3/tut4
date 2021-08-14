1f5a5fde4049a91adcc7baeab5457a4423fdf514
Fiona <fionacoulter3@gmail.com>
2021-08-14 22:00:05 +1200

    TestSubtract added

--- src/test/java/Tut4/TestCalc.java
+++ src/test/java/Tut4/TestCalc.java
@@ -6,8 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCalc{
 
-@Test
-public void testadd(){
-	assertEquals(3, Calc.add(1, 2));
-}
+	@Test
+	public void testadd(){
+		assertEquals(3, Calc.add(1, 2));
+	}
+
+	@Test
+	public void testadd() {
+		assertEquals(1, Calc.subtract(2, 1));
+	}
+
 }
